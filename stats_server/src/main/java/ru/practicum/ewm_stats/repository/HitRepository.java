622bf4d74da9e8555ce02956f43c0f93e10bb7cd
Elizaveta Shapovalova <elizaveta-shapovalova.sha@yandex.ru>
2023-02-17 13:41:30 +0400

    Fix query in HitRepository

--- stats_server/src/main/java/ru/practicum/ewm_stats/repository/HitRepository.java
+++ stats_server/src/main/java/ru/practicum/ewm_stats/repository/HitRepository.java
@@ -14,15 +14,17 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("SELECT new ru.practicum.ewm_stats.dto.ViewStats(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
             "FROM Hit h " +
-            "WHERE h.timestamp between ?1 AND ?2" +
-            "AND h.uri IN (?3)" +
-            "ORDER BY COUNT(DISTINCT h.ip) DESC")
+            "WHERE h.timestamp between ?1 AND ?2 " +
+            "AND h.uri IN (?3) " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC ")
     List<ViewStats> findDistinctViews(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.ewm_stats.dto.ViewStats(h.app, h.uri, COUNT(h.ip)) " +
             "FROM Hit h " +
-            "WHERE h.timestamp between ?1 AND ?2" +
-            "AND h.uri IN (?3)" +
-            "ORDER BY COUNT(h.ip) DESC")
+            "WHERE h.timestamp between ?1 AND ?2 " +
+            "AND h.uri IN (?3) " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h.ip) DESC ")
     List<ViewStats> findViews(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
